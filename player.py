8430f4ef05390cb9c09e557ec3c7f7d87d04530b
Peter Karakas <karakas.peter@gmail.com>
2019-02-02 10:08:46 +0100

    Add logs

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print(game_state)
         return 1000
 
     def showdown(self, game_state):
