ed49f2dc8198ba676fc3d9b56c9509ed7dba25d3
Peter Karakas <karakas.peter@gmail.com>
2019-02-02 10:53:08 +0100

    add log

--- player.py
+++ player.py
@@ -3,10 +3,10 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print("GAME DATA", game_state)
         if "players" in game_state.keys():
             for player in game_state['players']:
                 if "name" in player.keys() and player['name'] == "HalalPoker":
-                    print("Plaer DATA ", player)
                     if "hole_cards" in player.keys():
                         if len(player["hole_cards"]) >=2:
                             if player["hole_cards"][0]["rank"] == player["hole_cards"][1]["rank"]:
