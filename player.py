1c46133f1f76273da3de602adb5b8cc7ded0fbc4
Peter Karakas <karakas.peter@gmail.com>
2019-02-02 10:35:58 +0100

    Awesome strategy

--- player.py
+++ player.py
@@ -3,11 +3,16 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        # for player in game_state['players']:
-        #     print(player['name'])
-        print("Peti okos2")
-        print(game_state)
-        return 1000
+        if "players" in game_state.keys():
+            for player in game_state['players']:
+                if "name" in player.keys() and player['name'] == "HalalPoker":
+                    if "hole_cards" in player.keys():
+                        if len(player["hole_cards"]) >=2:
+                            if player["hole_cards"][0]["rank"] == player["hole_cards"][1]["rank"]:
+                                return 1000
+                            if (player["hole_cards"][0]["rank"] in ["10", "J", "Q", "K", "A"]) and (player["hole_cards"][1]["rank"] in ["10", "J", "Q", "K", "A"]):
+                                return 1000
+        return 0
 
     def showdown(self, game_state):
         pass
