0ab677cb5aac35deeac3ac8852774500d8a9b764
Zsolt Rábai <rabxly@gmail.com>
2019-02-02 16:17:08 +0100

    sor hozzáadva a card_strength-hez

--- player.py
+++ player.py
@@ -70,14 +70,48 @@ class Player:
 
     def calc_strength(self, game_state):
 
+        order_map1 = {
+            'A': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            'T': 10,
+            'J': 11,
+            'Q': 12,
+            'K': 13
+        }
+
+        order_map2 = {
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            'T': 10,
+            'J': 11,
+            'Q': 12,
+            'K': 13,
+            'A': 14
+        }
+
         hole_cards = game_state["players"][0]["hole_cards"]
         community_cards = game_state["community_cards"]
         colors = dict()
         ranks = dict()
+        rank_list = list()
         for card in hole_cards + community_cards:
             if card["rank"] not in ranks.keys():
                 ranks[card["rank"]] = 0
             ranks[card["rank"]] += 1
+            rank_list.append(card["rank"])
 
             if card["suit"] not in colors.keys():
                 colors[card["suit"]] = 0
@@ -85,8 +119,24 @@ class Player:
             colors[card["suit"]] += 1
         print(colors)
         print(ranks)
-        if max(colors.values()) > 4:
-            return 4
+
+        sample = ",".join([str(d) for d in range(1, 15)])
+        card_line_1 = [order_map1.get(c) for c in rank_list]
+        card_line_1.sort()
+        card_line_2 = [order_map2.get(c) for c in rank_list]
+        card_line_2.sort()
+        card_line_joined = list()
+        if len(card_line_1) == 5:
+            card_line_joined.append(card_line_1)
+            card_line_joined.append(card_line_2)
+        if len(card_line_1) == 6:
+            for i in range(2):
+                card_line_joined.append(card_line_1[i:i + 5])
+                card_line_joined.append(card_line_2[i:i + 5])
+        if len(card_line_1) == 7:
+            for i in range(3):
+                card_line_joined.append(card_line_1[i:i + 5])
+                card_line_joined.append(card_line_2[i:i + 5])
 
         two_same = 0
         three_same = 0
@@ -99,9 +149,16 @@ class Player:
             if val == 4:
                 four_same += 1
         if four_same == 1:
-            return 6
+            return 7
         if (three_same == 1) & (two_same == 1):
+            return 6
+        if max(colors.values()) > 4:
             return 5
+        for card_line in card_line_joined:
+            s = ",".join([str(c) for c in card_line])
+            if s in sample:
+                print(s)
+                return 4
         if (three_same == 1) & (two_same == 0):
             return 3
         if two_same == 2:
