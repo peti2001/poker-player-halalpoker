9cfe8d383bda8587c444a7a808f83b9f4ddb86c8
Peter Karakas <karakas.peter@gmail.com>
2019-02-02 12:03:31 +0100

    add logging

--- player.py
+++ player.py
@@ -4,7 +4,8 @@ class Player:
 
     def betRequest(self, game_state):
         print("GAME DATA", game_state)
-        print("Minimum raise:", game_state["minimum_raise"])
+        if "minimum_raise" is game_state:
+            print("Minimum raise:", game_state["minimum_raise"])
         if "players" in game_state.keys():
             for player in game_state['players']:
                 if "name" in player.keys() and player['name'] == "HalalPoker":
@@ -17,7 +18,7 @@ class Player:
                                 print("We have high cards. ALL IN", player["hole_cards"])
                                 
                                 return 1000
-        if int(game_state["minimum_raise"]) < 100:
+        if "minimum_raise" in game_state and int(game_state["minimum_raise"]) < 100:
             print("Kicsi emeles", player["hole_cards"])
             return game_state["minimum_raise"] + 2 * game_state["big_blind"]
         return 0
